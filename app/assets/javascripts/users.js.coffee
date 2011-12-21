04cd942529539fe9719969c8b7f5471b0140dc6c
Mawi Marín <mawitu@gmail.com>
2011-12-21 16:55:11 +0100

    Adding custom validations

--- app/assets/javascripts/users.js.coffee
+++ app/assets/javascripts/users.js.coffee
@@ -4,15 +4,26 @@
 
 $(document).ready ->
 
-  jQuery.validator.addMethod "complete_uri", (val,elem) ->
-    /(smb|nfs|ftp):\/\/([\S]*)\/.*/.test(val);
+  # jQuery.validator.addMethod "complete_uri", (val,elem) ->
+  #   /(smb|nfs|ftp):\/\/([\S]*)\/.*/.test(val);
+  `jQuery.validator.addMethod("uri", function(val, elem) {
+    return /(smb|nfs|ftp):\/\/([\S]*)\/.*/.test(val);
+  }, "Please enter valid URI");`
 
   # jQuery.validator.addMethod "ip", (val,elem) ->
   #    /([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))$/i.test(val);
   `jQuery.validator.addMethod("ip", function(val, elem) {
     return /([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))$/i.test(val);
-  }, "Insert a valid IP number");`
+  }, "Please enter a valid IP x.x.x.x");`
+
+  `jQuery.validator.addMethod("integer", function(val, elem) {
+    return (val > 0 && val < 65536);
+  }, "Please enter a valid number between 0 and 65335");`
+
   $("#edit_user").validate
     rules:
         ip: "ip"
-        uri: "complete_uri"
+        uri: "uri"
+        integer:
+          numeric: true
+          integer: true
---
+++ app/helpers/home_users_helper.rb
@@ -0,0 +1,71 @@
+module HomeUsersHelper
+def render_fieldset(field,data)
+  out =  "<fieldset> <legend> #{field[0]}"
+  out << "(Multiple)" if !field[1][:principal].blank?
+  out << "</legend>"
+
+  if !field[1][:principal].blank?
+    data[field[0]].each_with_index do |value,index|
+      field[1][:attributes].each do |x|
+        out << render_attribute(x.keys.first, x.values.first, value[x.keys.first.split("/")[2]], index)
+      end
+    end
+  else
+
+  field[1][:attributes].each do |x|
+    out << render_attribute(x.keys.first, x.values.first, data[x.keys.first.split("/")[1]])
+  end
+end
+
+  out << "</fieldset><br/>"
+end
+
+def render_attribute(key,properties,data,index = nil)
+   size = case data.size
+   when 0..10
+     out = "<p class = 'short'>"
+   when 10..40
+     out = "<p class = 'medium'>"
+   when 40..80
+     out = "<p class = 'long'>"
+   when 80..200
+     out = "<p class = 'giant'>"
+   else
+     out = "<p>"
+   end
+
+   display_label = properties["display_name"]
+   display_label += "* " if properties["required"]
+   input_class = "required"if properties["required"]
+
+   out << label_tag(key, display_label)
+
+   if index.nil?
+     field_id = key.split("/").map{|x| "[#{x}]"}.join
+   else
+     field_id = key.split("/").map{|x| "[#{x}]"}.insert(2, "[#{index}]").join
+   end
+    field_id = "[databag]"+ field_id
+
+   if !properties["choice"].blank?
+     out << select_tag(field_id, options_for_select(properties["choice"], data))
+   # elsif properties["html_type"] == "url"
+   #   input_class += " url"
+   #   out << text_field_tag(field_id, data, :class => input_class)
+   # elsif properties["html_type"] == "uri"
+   #   input_class += " uri"
+   #   out << text_field_tag(field_id, data, :class => input_class)
+   # elsif properties["html_type"] == "number"
+   #   input_class += " number"
+   #   out << number_field_tag(field_id, data, :class => input_class)
+   else
+     input_class += " #{properties["validation"]}"
+     out << text_field_tag(field_id, data, :class => input_class)
+   end
+
+   out << "<p class = 'hint'>#{properties['description']}</p></p>"
+
+
+end
+
+end
